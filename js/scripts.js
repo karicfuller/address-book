fc7fb68684671d146e6da18605f528ff481a17d3
Kari Fuller <kari.c.fuller@gmail.com>
2018-06-27 17:26:17 -0700

    add to business logic with contact and address constructors

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,19 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresss = [];
 }
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
 
 //ui logic
 
@@ -16,7 +28,14 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span</li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span</li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
